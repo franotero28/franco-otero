5fe163eca00cb122dfb963f0c14b56850b36ad96
Franco Otero <fran28otero10kochur03@gmail.com>
2023-08-12 15:29:14 -0300

    JS spicy

---
+++ Projects/La-picante/js/data.js
@@ -0,0 +1,14 @@
+export const events = [
+    {
+    id: 1,
+    title: "Evento 23 de septiembre",
+    description: "La joda mas epica de todas nasheee",
+    date: "23/09/23",    
+},
+{
+    id: 2,    
+    title: "Evento fin de año 2023",
+    description: "Joda de fin de año la mas PICANTE de todas",
+    date: 31/12/23
+}
+]
