6351bf86e1f13bf65fe25b82a6b5474cc06c6e3f
Franco Otero <fran28otero10kochur03@gmail.com>
2023-09-11 15:53:57 -0300

    reserva system

--- reserva turnos/index.js
+++ reserva turnos/index.js
@@ -2,15 +2,40 @@ import {days} from "/data.js"
 
 const verTurnos = document.getElementById("verTurnos")
 const boton = document.getElementById("mostrarTurnos")
+let botonPresionado = false;
 
-boton.addEventListener("click", ()=>
-days.forEach((turno) => {
+
+
+
+boton.addEventListener("click", () => {
+    if (!botonPresionado) {
+        botonPresionado = true;
+        days.forEach((turno) => {
+            let content = document.createElement("div");
+            content.className = "div-turnos";
+            content.innerHTML = `
+                <p style="margin-right:10px;">${turno.day}</p>
+                <p>${turno.hour}hs</p>  
+            `;
+            verTurnos.append(content);
+
+            let reservarbtn = document.createElement("button")
+            reservarbtn.className = "btn btn-primary"
+            reservarbtn.innerHTML= `Reservar`;
+            content.append(reservarbtn)    
+        });
+    }
+});
+
+
+
+/*boton.addEventListener("click", ()=> {
+    days.forEach((turno) => {
     let content = document.createElement("div");
-    content.innerHTML = ""
     content.className = "div-turnos"
     content.innerHTML = `
     <p style="margin-right:10px;">${turno.day}</p>
     <p>${turno.hour}hs</p>  
     `;
     verTurnos.append(content)
-}))
+})})*/
