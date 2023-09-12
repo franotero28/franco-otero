a746fc7e504f4227f051b9075afcd1b3b5c3a79f
Franco Otero <fran28otero10kochur03@gmail.com>
2023-09-12 16:10:31 -0300

    sistema reservas

--- reserva turnos/index.js
+++ reserva turnos/index.js
@@ -1,7 +1,7 @@
 import {days} from "/data.js"
 
-const verTurnos = document.getElementById("verTurnos")
-const boton = document.getElementById("mostrarTurnos")
+const turnosLunes = document.getElementById("turnosLunes")
+const boton = document.getElementById("botonLunes")
 let botonPresionado = false;
 
 boton.addEventListener("click", () => {
@@ -14,11 +14,12 @@ boton.addEventListener("click", () => {
                 <p>${turno.day}</p>
                 <p>${turno.hour}hs</p>
             `;
-            verTurnos.append(content);
+            turnosLunes.append(content);
 
             let reservarbtn = document.createElement("button")
             reservarbtn.className = "btn btn-primary"
             reservarbtn.innerHTML= `Reservar`;
+            reservarbtn.id = "1"
             content.append(reservarbtn)    
         });
     }
@@ -34,5 +35,5 @@ boton.addEventListener("click", () => {
     <p style="margin-right:10px;">${turno.day}</p>
     <p>${turno.hour}hs</p>  
     `;
-    verTurnos.append(content)
+    turnosLunes.append(content)
 })})*/
