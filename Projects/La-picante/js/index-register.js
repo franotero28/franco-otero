5df4dcbcfd4ee0daaaffd7c49142c9e396dbdcf8
Franco Otero <fran28otero10kochur03@gmail.com>
2023-09-29 16:08:28 -0300

    register in process

---
+++ Projects/La-picante/js/index-register.js
@@ -0,0 +1,12 @@
+const user = document.getElementById("user")
+const password = document.getElementById("password")
+AOS.init()
+
+/*function newLogin(){
+    const usuario = (user.value)
+    const contraseña = (password.value)
+    console.log(usuario)
+    console.log(contraseña)
+}*/
+
+
