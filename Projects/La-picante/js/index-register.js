b64eb78105104daa2992ff543c9efb98c86cb1a2
Franco Otero <fran28otero10kochur03@gmail.com>
2023-09-30 13:53:15 -0300

    30/9

--- Projects/La-picante/js/index-register.js
+++ Projects/La-picante/js/index-register.js
@@ -2,11 +2,10 @@ const user = document.getElementById("user")
 const password = document.getElementById("password")
 AOS.init()
 
-/*function newLogin(){
+function newLogin(){
     const usuario = (user.value)
     const contraseña = (password.value)
     console.log(usuario)
     console.log(contraseña)
-}*/
-
+}
 
