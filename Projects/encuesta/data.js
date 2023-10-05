7fa7b1426be35c5cb1ac10aab30832ccf89d84a2
Franco Otero <fran28otero10kochur03@gmail.com>
2023-10-05 16:58:52 -0300

    encuesta

---
+++ Projects/encuesta/data.js
@@ -0,0 +1,14 @@
+export const questions = [
+    {
+    question: "Como es el nombre de la fiesta?",
+    option1: "La picante fest",
+    option2: "La Picante",
+    option3: "Los Picantes",
+    },
+    {
+    question: "Como se llama el organizador?",
+    option1: "Henry",
+    option2: "Lucas",
+    option3: "Coscu"
+}
+]
--- Projects/encuesta/index.js
+++ Projects/encuesta/index.js
@@ -1,14 +1,19 @@
-const boton1 = document.getElementById("1")
 const btnName = document.getElementById("btnName")
 const input = document.getElementById("name")
 const nombreContainer = document.getElementById("nombreContainer")
+const eDatos = document.getElementById("eDatos")
+const preg1 = document.getElementById("preg1")
 
+import { questions } from "./data.js"
 
 btnName.addEventListener("click", ()=>{
     let nombre = input.value
     input.value = ""
     nombreContainer.innerHTML = `${nombre}`
-    input.style.display = "hidden"
+    eDatos.style.display = "none"
+    preg1.style.display = "block"
 })
 
-
+questions.forEach((pregunta) =>{
+    const title = document.createElement("li")
+})
