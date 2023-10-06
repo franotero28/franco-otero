8cb3dfa1c0c1b60f2a8e89988042e32492da779a
Franco Otero <fran28otero10kochur03@gmail.com>
2023-10-06 14:12:24 -0300

    encuestas

--- Projects/encuesta/data.js
+++ Projects/encuesta/data.js
@@ -7,8 +7,26 @@ export const questions = [
     },
     {
     question: "Como se llama el organizador?",
-    option1: "Henry",
-    option2: "Lucas",
+    option1: "Lucas",
+    option2: "Henry",
     option3: "Coscu"
-}
+    },
+    {
+    question: "Cuando fue nuestra primer fiesta?",
+    option1: "2019",
+    option2: "2020",
+    option3: "2021"
+    },
+    {
+    question: "Donde fue nuestra primera fiesta?",
+    option1: "En una casa",
+    option2: "En laguna",
+    option3: "Garibaldi"
+    },
+    {
+    question: "Que dia va a ser la fiesta de halloween en laguna?",
+    option1: "28 de octubre",
+    option2: "4 de noviembre",
+    option3: "11 de noviembre"
+    },
 ]
--- Projects/encuesta/index.js
+++ Projects/encuesta/index.js
@@ -8,12 +8,45 @@ import { questions } from "./data.js"
 
 btnName.addEventListener("click", ()=>{
     let nombre = input.value
-    input.value = ""
+    if(nombre === ""){
+        alert("Ingrese nombre valido")
+    }
+    else{ 
     nombreContainer.innerHTML = `${nombre}`
     eDatos.style.display = "none"
     preg1.style.display = "block"
-})
+    }
+});
 
 questions.forEach((pregunta) =>{
-    const title = document.createElement("li")
+    const preguntas = document.createElement("h6")
+    preguntas.className = "text-center"
+    preguntas.innerHTML = `${pregunta.question}`
+    const liPreguntas = document.createElement("ul")
+    liPreguntas.className = "ul-preguntas"
+    liPreguntas.innerHTML = 
+    `<li class="option"><button class="btn btn-primary p-2 m-1" id="1">${pregunta.option1}</button><input class="form-check-input" type="checkbox" value="" id="myCheck1"></li>
+    <li class="option"><button class="btn btn-primary p-2 m-1" id="2">${pregunta.option2}</button><input class="form-check-input" type="checkbox" value="" id="myCheck2"></li>
+    <li class="option"><button class="btn btn-primary p-2 m-1" id="3">${pregunta.option3}</button><input class="form-check-input" type="checkbox" value="" id="myCheck3"></li>`
+
+    preg1.append(preguntas, liPreguntas)
+});
+
+const checkbox1 = document.getElementById("myCheck1")
+const checkbox2 = document.getElementById("myCheck2")
+const checkbox3 = document.getElementById("myCheck3")
+
+checkbox1.addEventListener("click", () =>{
+    checkbox2.checked = false
+    checkbox3.checked = false
+})
+
+checkbox2.addEventListener("click", () =>{
+    checkbox1.checked = false
+    checkbox3.checked = false
+})
+
+checkbox3.addEventListener("click", () =>{
+    checkbox1.checked = false
+    checkbox2.checked = false
 })
