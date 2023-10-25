decccaf2b5eb583cc180289490d8eaa0fa804802
Franco Otero <fran28otero10kochur03@gmail.com>
2023-10-25 16:51:08 -0300

    balduneta e-sports

--- Projects/balduneta/index.js
+++ Projects/balduneta/index.js
@@ -1,1 +1,20 @@
 AOS.init();
+import {games} from "./data.js"
+const divtrack = document.getElementById("tracks")
+
+
+games.forEach((game) => {
+    const tracker = document.createElement("div")
+    tracker.className = "tracker-games"
+    tracker.innerHTML = `
+    <img src="${game.img}" width="60">
+    <p class="m-2">${game.team1}</p>
+    <p class="m-2">${game.rondaB}</p>
+    <p class="m-2">-</p>
+    <p class="m-2">${game.rondaR}</p>
+    <p class="m-2">${game.team2}</p>
+    <img src="${game.img2}" width="60">
+    `
+
+    divtrack.append(tracker)
+})
