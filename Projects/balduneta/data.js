f943326c7dd85621da727de1c3fc4d03ede5f796
Franco Otero <fran28otero10kochur03@gmail.com>
2023-10-26 16:13:51 -0300

    balduneta e-sports

--- Projects/balduneta/data.js
+++ Projects/balduneta/data.js
@@ -5,6 +5,16 @@ export const games = [
     rondaR: 13,
     team2: "Los Polludos",
     img: "https://i.postimg.cc/gJJmJw94/oso-balduneta.webp",
-    img2: "https://i.postimg.cc/x8q08Xmn/lospoulludo.png"
-    }
+    img2: "https://i.postimg.cc/x8q08Xmn/lospoulludo.png",
+    win: false,
+    },
+    {
+    team1: "La Balduneta",
+    rondaB: 13,
+    rondaR: 9,
+    team2: "Tortuguitas anashe",
+    img: "https://i.postimg.cc/gJJmJw94/oso-balduneta.webp",
+    img2: "https://i.postimg.cc/tg4838Lp/image2.webp",
+    win: true,
+    },
 ]
--- Projects/balduneta/index.js
+++ Projects/balduneta/index.js
@@ -2,19 +2,19 @@ AOS.init();
 import {games} from "./data.js"
 const divtrack = document.getElementById("tracks")
 
-
+let i = 0
 games.forEach((game) => {
     const tracker = document.createElement("div")
-    tracker.className = "tracker-games"
+    tracker.className = "tracker-games" + i
     tracker.innerHTML = `
     <img src="${game.img}" width="60">
-    <p class="m-2">${game.team1}</p>
-    <p class="m-2">${game.rondaB}</p>
+    <p class="team-name m-2">${game.team1}</p>
+    <p class="rondas-teams m-2">${game.rondaB}</p>
     <p class="m-2">-</p>
-    <p class="m-2">${game.rondaR}</p>
-    <p class="m-2">${game.team2}</p>
+    <p class="rondas-teams m-2">${game.rondaR}</p>
+    <p class="team-name m-2">${game.team2}</p>
     <img src="${game.img2}" width="60">
     `
-
+    i = i + 1
     divtrack.append(tracker)
 })
