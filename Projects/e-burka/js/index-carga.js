d7315670e517ce24404479f418c2482382967e22
Franco Otero <fran28otero10kochur03@gmail.com>
2023-12-02 16:51:38 -0300

    modal hecho

--- Projects/e-burka/js/index-carga.js
+++ Projects/e-burka/js/index-carga.js
@@ -17,13 +17,36 @@ articulos.forEach((elemento) =>{
     divGaleria.append(divArticulo)
     
     divArticulo.addEventListener("click", ()=>{
-        const modal = document.createElement("div")
-        modal.className= "ventana-modal"
-        modal.innerHTML=`
-        <img class="img-articulo" src="${elemento.img}" width="250" height="200">
-        <p class="nombre-articulo">${elemento.nombre} ${elemento.descripcion} </p>
-        <p class="precio-articulo">$${elemento.precio}</p>
+        // Eliminar el modal existente si hay uno
+        const modalExistente = document.querySelector(".div-modal");
+        if (modalExistente) {
+        modalExistente.remove();
+        }
+        const modal = document.createElement("div");
+        modal.className = "div-modal"
+        const modalHeader = document.createElement("div")
+        modalHeader.className = "modal-h"
+        modalHeader.innerHTML =`
+        <img src="/img/logo-zeus-horizontal.png" width="200"><button class="btn-cerrar btn btn-danger" id="cerrarModal">x</button>`
+
+        const modalContent = document.createElement("div")
+        modalContent.className= "ventana-modal"
+        modalContent.innerHTML=`
+        <img class="img-articulo" src="${elemento.img}" width="250" height="210">
+        <div class="descripcion-modal">
+        <p class="nombre-modal">${elemento.nombre} ${elemento.descripcion} </p>
+        <p class="precio-modal">$${elemento.precio}</p>
+        <button class="btn btn-light">Comprar</button>
+        </div>
         `
+        modal.append(modalHeader)
+        modal.append(modalContent)
         divModal.append(modal)
+
+        const cerrarModal = document.getElementById("cerrarModal");
+        cerrarModal.addEventListener("click", () => {
+        modal.remove(); // Elimina el modal actual en lugar de ocultarlo
+        });
     })
 })
+
--- Projects/e-burka/js/index-perifericos.js
+++ Projects/e-burka/js/index-perifericos.js
@@ -1,6 +1,7 @@
 import { articulos } from "./data-perifericos.js";
 
 const divGaleria = document.getElementById("galeria")
+const divModal = document.getElementById("modal")
 let indice = 0
 
 articulos.forEach((elemento) =>{
@@ -8,10 +9,43 @@ articulos.forEach((elemento) =>{
     divArticulo.className = "div-articulo"
     divArticulo.id = indice++
     divArticulo.innerHTML = `
-    <img class="img-articulo" src="${elemento.img}" width="150">
+    <img class="img-articulo" src="${elemento.img}" width="150" height="160">
     <hr class="border border-1 opacity-100 w-100">
     <p class="nombre-articulo">${elemento.nombre} ${elemento.descripcion} </p>
     <p class="precio-articulo">$${elemento.precio}</p>
     `
     divGaleria.append(divArticulo)
+    
+    divArticulo.addEventListener("click", ()=>{
+        // Eliminar el modal existente si hay uno
+        const modalExistente = document.querySelector(".div-modal");
+        if (modalExistente) {
+        modalExistente.remove();
+        }
+        const modal = document.createElement("div");
+        modal.className = "div-modal"
+        const modalHeader = document.createElement("div")
+        modalHeader.className = "modal-h"
+        modalHeader.innerHTML =`
+        <img src="/img/logo-zeus-horizontal.png" width="200"><button class="btn-cerrar btn btn-danger" id="cerrarModal">x</button>`
+
+        const modalContent = document.createElement("div")
+        modalContent.className= "ventana-modal"
+        modalContent.innerHTML=`
+        <img class="img-articulo" src="${elemento.img}" width="250" height="210">
+        <div class="descripcion-modal">
+        <p class="nombre-modal">${elemento.nombre} ${elemento.descripcion} </p>
+        <p class="precio-modal">$${elemento.precio}</p>
+        <button class="btn btn-light">Comprar</button>
+        </div>
+        `
+        modal.append(modalHeader)
+        modal.append(modalContent)
+        divModal.append(modal)
+
+        const cerrarModal = document.getElementById("cerrarModal");
+        cerrarModal.addEventListener("click", () => {
+        modal.remove(); // Elimina el modal actual en lugar de ocultarlo
+        });
+    })
 })
