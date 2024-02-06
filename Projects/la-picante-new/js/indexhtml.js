10e4f3d653f276fe28ba985045cd6994ea92661f
Franco <fran28otero10kochur03@gmail.com>
2024-02-06 20:32:17 -0300

    flyer proxima joda

--- Projects/la-picante-new/js/indexhtml.js
+++ Projects/la-picante-new/js/indexhtml.js
@@ -2,6 +2,7 @@ AOS.init();
 
 const crearCuenta = document.getElementById("crearCuenta")
 const botonReserva = document.getElementById("boton-reserva")
+const comprarEntradas = document.getElementById("comprarEntradas")
 
 crearCuenta.addEventListener("click", ()=>{
   Swal.fire({
@@ -16,6 +17,12 @@ crearCuenta.addEventListener("click", ()=>{
 
 botonReserva.addEventListener("click", ()=>{
   console.log("se apreto el boton")
+  let whpplink = "https://api.whatsapp.com/send?phone=2233048014&text=Hola,%20te%20escribo%20para%20reservar%20un%20turno%20para%20barberia"
+  window.open(whpplink, '_blank');
+})
+
+
+comprarEntradas.addEventListener("click", ()=>{
   let whpplink = "https://api.whatsapp.com/send?phone=2233048014&text=Hola,%20te%20escribo%20para%20reservar%20un%20turno%20para%20barberia"
   window.open(whpplink, '_blank');
 })
