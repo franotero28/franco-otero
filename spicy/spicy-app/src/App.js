d57420123b0bd7591249c2c6d9323f8323093bb3
Franco <fran28otero10kochur03@gmail.com>
2024-03-15 02:18:09 -0300

    spicy app

--- spicy/spicy-app/src/App.js
+++ spicy/spicy-app/src/App.js
@@ -1,13 +1,16 @@
 import logo from './img/lapicante.png';
 import './App.css';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Alata&family=Quicksand:wght@700&display=swap')
+</style>
 
 function App() {
   return (
     <div className="App">
       <div className='contenedor-principal'>
         <div className='contenedor-header'>
+          <h1 className='h1-spicy alata-regular quicksand'>Spicy</h1>
           <img src={logo} className='logo-picante'/>
-          <h1 className='h1-spicy'>Spicy</h1>
         </div>
         <div className='contenedor-buscador'>
           <input className='buscador' type='search' placeholder='Boliches, Bares, Fiestas, etc'></input>
