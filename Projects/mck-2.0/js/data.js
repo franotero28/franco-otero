831efe8709279217cc0682ea2fbeb524b36fa8fb
Franco <fran28otero10kochur03@gmail.com>
2024-04-17 20:46:42 -0300

    ma 2.0

---
+++ Projects/mck-2.0/js/data.js
@@ -0,0 +1,11 @@
+export const archivos = [
+    {
+        nombre: "Reiki",
+        descarga: "https://drive.google.com/file/d/15gc7zsVP2lyRNwRyKvlg5duivGXLe7-M/view?usp=sharing"
+    },
+    {
+        nombre: "Manzana",
+        descarga: "https://drive.google.com/file/d/15gc7zsVP2lyRNwRyKvlg5duivGXLe7-M/view?usp=sharing"
+    }
+
+] 
---
+++ Projects/mck-2.0/js/index.js
@@ -0,0 +1,25 @@
+const botonArchivos = document.getElementById("botonArchivos")
+const divMaterial = document.getElementById("contenedor4")
+
+import { archivos } from "./data.js";
+
+
+
+botonArchivos.addEventListener("click", ()=> {
+    let contenedorArchivos = document.createElement("div");
+    divMaterial .innerHTML = ` `
+    contenedorArchivos.className= "contenedor-archivos";
+
+    divMaterial.append(contenedorArchivos);
+
+    archivos.forEach((archivos) =>{
+        const contenedorMaterial = document.createElement("div")
+        contenedorMaterial.className = "contenedor-material"
+        contenedorMaterial.innerHTML = `
+        <h6>Archivos ${archivos.nombre}</h6>
+        <p style="font-size: 100px; cursor: pointer;">📄</p>
+        <a class="boton-descarga" target="_blank" href="${archivos.descarga}" download>Descargar</a>`
+
+      contenedorArchivos.append(contenedorMaterial);
+    })
+})
