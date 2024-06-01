ca88923bb043e76cccea03c8b3f546679208f84e
Franco <fran28otero10kochur03@gmail.com>
2024-06-01 13:20:16 -0300

    errors fix

--- Projects/app-pescaderia/src/components/formulario.jsx
+++ Projects/app-pescaderia/src/components/formulario.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import {useForm} from "react-hook-form"
 
 function Formulario(){
+
+    const {register, handleSubmit, formState: {errors}} = useForm()
+    const onSubmit = handleSubmit((data) => { 
+        console.log(data)
+      })
+
     return(
-        <ContenedorFormulario>
+        <ContenedorFormulario onSubmit={onSubmit}>
         <img className="img-login" src={require("../img/2_.jpeg")} alt="" />
+        <form onSubmit={onSubmit}>
         <div className="form-floating m-3 w-100">
             <select className="form-select" id="usuario" >
                 <option value="0" selected></option>
@@ -15,10 +23,26 @@ function Formulario(){
             <label for="usuario">Usuario</label>
         </div>
         <div className="form-floating m-3 w-100">
-            <input type="password" className="form-control" id="#" placeholder="Contraseña" />
-            <label for="#">Contraseña</label>
-        </div>
-        <button className="btn btn-primary m-3 w-100">Ingresar</button>
+            <input type="password" className="form-control" id="contraseña" placeholder="Contraseña"{...register("password", {
+                        required:{
+                            value: true,
+                            message: "Debe ingresar contraseña valida"
+                        },
+                        minLength: { 
+                        value:6,
+                        message: "La contraseña debe tener min 6 caracteres"},
+                        maxLength:{
+                        value:10,
+                        message:"La contraseña debe tener max 10 caracteres"
+                        }
+                    })} />
+                    {
+                        errors.password && <span>{errors.password.message}</span>
+                    }
+            <label for="contraseña">Contraseña (min 6 caracteres)</label>
+        </div> 
+        <button type="submit" className="btn btn-primary m-3 w-100">Ingresar</button>
+        </form>
         </ContenedorFormulario>
     )
 }
@@ -33,10 +57,22 @@ const ContenedorFormulario = styled.div`
     justify-content:space-around;
     align-items:center;
     
+    form{
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+        align-items:center;
+    }
+
     .img-login{
         width:100px;
         border-radius:50px;
     }
+    span {
+    font-size: 11px;
+    padding:2px;
+    color: #ca6b6b;
+}
 `
 
 export default Formulario
