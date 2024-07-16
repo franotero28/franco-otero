2da109b598dfe0eda1f21e2db0a5831edd61d95f
Franco <fran28otero10kochur03@gmail.com>
2024-07-16 19:38:38 -0300

    node changes

---
+++ Projects/mck-project/mck-node/index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import cors from "cors"
+import db from "./database/db.js"
+import blogRoutes from "./routes/routes.js"
+
+const app = express()
+
+app.use( cors())
+app.use(express.json())
+app.use('/blogs', blogRoutes)
+
+const port = process.env.PORT || 3000
+
+try {
+    await db.authenticate()
+    console.log("conexion exitosa")
+} catch (error) {
+    console.log(`el error de conexion es : ${error}`)
+}
+
+app.get("/", (req,res)=>{
+    res.send("hola mundo")
+})
+
+app.listen(port, ()=>{
+    console.log(`server levantado in http://localhost:${port}`)
+})
+
+
